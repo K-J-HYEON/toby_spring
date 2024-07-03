1bd2f796c30715470c75622d857f12932ea106b3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-04 02:53:37 +0900

    ObjectFactory 수정

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -2,10 +2,13 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.exrate.SimpleExRateProvider;
+import tobyspring.hellospring.payment.PaymentService;
 
 @Configuration
 public class ObjectFactory {
-
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
