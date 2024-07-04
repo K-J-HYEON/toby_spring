ce375dc3254651f142d2dfe3c27c77f78e53ce20
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-04 18:17:02 +0900

    ClockTest 반영

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -7,15 +7,20 @@ import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.SimpleExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
 
+import java.time.Clock;
+
 @Configuration
 public class ObjectFactory {
     @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
+    public PaymentService paymentService() { return new PaymentService(exRateProvider(), clock());}
 
     @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
     }
 }
