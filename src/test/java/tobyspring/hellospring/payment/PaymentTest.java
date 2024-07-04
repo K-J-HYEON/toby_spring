83d9435d0c674a96bef8d46fc9c692e3e60d1bfb
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-04 18:51:30 +0900

    PaymentTest 수정

--- src/test/java/tobyspring/hellospring/payment/PaymentTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentTest.java
@@ -2,7 +2,6 @@ package tobyspring.hellospring.payment;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.Duration;
@@ -10,8 +9,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-
-import static java.lang.String.valueOf;
 import static org.assertj.core.api.Assertions.*;
 
 public class PaymentTest {
@@ -20,7 +17,7 @@ public class PaymentTest {
         Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
 
         Payment payment = Payment.createPrepared(
-                1L, "USD", BigDecimal.TEN,valueOf(1_000), LocalDateTime.now(clock)
+                1L, "USD", BigDecimal.TEN, BigDecimal.valueOf(1_000), LocalDateTime.now(clock)
         );
 
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
@@ -32,7 +29,7 @@ public class PaymentTest {
         Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
 
         Payment payment = Payment.createPrepared(
-                1L, "USD", BigDecimal.TEN, valueOf(1_000), LocalDateTime.now(clock)
+                1L, "USD", BigDecimal.TEN, BigDecimal.valueOf(1_000), LocalDateTime.now(clock)
         );
 
         Assertions.assertThat(payment.isValid(clock)).isTrue();
