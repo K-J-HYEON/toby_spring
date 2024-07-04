e0ff4031196943905134c46e2920f051c60fed72
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-04 18:17:31 +0900

    PaymentServiceSpringTest 반영

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,56 @@
+package tobyspring.hellospring.payment;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.TestPaymentConfig;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestPaymentConfig.class)
+class PaymentServiceSpringTest {
+    @Autowired
+    PaymentService paymentService;
+    @Autowired
+    Clock clock;
+    @Autowired
+    ExRateProviderStub exRateProviderStub;
+
+    @Test
+    void convertedAmount() throws IOException {
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+
+        // exRate: 500
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    @Test
+    void validUntil() throws IOException {
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // valid util이 prepare() 30분 뒤로 설정됐는가?
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+
+    }
+}
