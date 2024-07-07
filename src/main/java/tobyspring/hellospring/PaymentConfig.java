aadc600c924aa6a7bc0fd266e023dbad536e8d43
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-07 14:16:05 +0900

    PaymentConfig 수정

---
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -0,0 +1,32 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.time.Clock;
+
+@Configuration
+public class PaymentConfig {
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider(), clock());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new JdkClientHttpRequestFactory());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new RestTemplateExRateProvider(restTemplate());
+    }
+
+    @Bean
+    public Clock clock() { return Clock.systemDefaultZone(); }
+}
