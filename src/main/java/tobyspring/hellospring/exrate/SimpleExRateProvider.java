166ff68621d3c2a7634c062c01dc378218dcbee4
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-11 16:16:01 +0900

    SimpleExRateProvider 수정

--- src/main/java/tobyspring/hellospring/exrate/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/SimpleExRateProvider.java
@@ -4,12 +4,18 @@ import tobyspring.hellospring.payment.ExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URISyntaxException;
 
 public class SimpleExRateProvider implements ExRateProvider {
     @Override
-    public BigDecimal getExRate(BigDecimal currency) throws IOException {
+    public BigDecimal getExRate(BigDecimal currency) {
         if (currency.equals("USD")) return BigDecimal.valueOf(1000);
 
         throw new IllegalArgumentException("지원되지 않는 통화");
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException, URISyntaxException {
+        return null;
     }
 }
